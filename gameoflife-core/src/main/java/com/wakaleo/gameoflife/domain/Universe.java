2ff6e80075717642ceb87a81366f27a1de72ae9b
shivakarthikd <52204995+shivakarthikd@users.noreply.github.com>
2019-09-04 06:56:13 +0000

    Update Universe.java

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Universe.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Universe.java
@@ -14,7 +14,7 @@ import static com.wakaleo.gameoflife.domain.Cell.DEAD_CELL;
  * Each new grid is generated from the previous one using the rules of the Game Of Life.
  */
 public class Universe {
-    private static String password="Karthik"
+    private static String password="Karthik";
     private static final String NEW_LINE = System.getProperty("line.separator");
 
     private Grid currentGridContent;
